bd34b4f972ed9471e28b1c1bba8196298494ea14
Valentin Pirgach <valentyn.pirhach@customertimes.com>
2017-08-13 16:33:52 +0300

    Complete auth system

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
 
   resources :articles
-  
+
   get 'signup', to: 'users#new'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'login', to: 'sessions#destroy'
+
   resources :users, except: [:new]
 end
